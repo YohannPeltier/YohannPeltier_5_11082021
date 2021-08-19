631936586723c433eb9644e2da5b06b10a282007
Yohann Peltier <yohannpeltier@gmail.com>
2021-08-19 20:08:06 +0200

    feat(index): add allproducts render

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,16 +1,27 @@
-// display of all products
-function viewItems(data) {
+let items = {};
+
+const saveData = (data) => {
+	items = data;
+	render();
+};
+
+// Show all products
+const render = () => {
 	const container = document.getElementById("container");
-	for (item of data) {
-		console.log(item);
-
-		let card = `<a class="card" href="item.html?name=${item.name}">
-                        <img class="card__img" src="${item.imageUrl}" alt="${item.name}"/>
-                        <span class="card__name">${item.name}</span>
-                        <span class="card__price">${item.price}</span>
-                        <p class="card__description">${item.description}</p>
-                    <a/>`;
-
-		container.innerHTML += card;
+	for (item of items) {
+		let card = `<div class="col">
+						<a class="card text-dark text-decoration-none shadow" href="item.html?id=${item._id}">
+							<img class="card-img-top" src="${item.imageUrl}" alt="${item.name}"/>
+							<div class="card-body">
+								<h4 class="card-title">${item.name}</h4>
+								<h5 class="card-title">${item.price.viewPrice()}</h5>
+								<p class="card-text">${item.description}</p>
+							</div>
+						</a>
+					</div>`;
+		container.insertAdjacentHTML("beforeend", card);
 	}
-}
+};
+
+// Render all products
+callApi(apiUrl, "", saveData);
