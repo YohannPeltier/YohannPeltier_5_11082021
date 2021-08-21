5fcd16b0ea01b4af4b1ff04bc16432704af3ede2
Yohann Peltier <yohannpeltier@gmail.com>
2021-08-21 09:57:16 +0200

    fix(cart): fix svg click event

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -144,10 +144,9 @@ const render = () => {
 								<td class="d-block d-md-table-cell table__td text-end" id="pi_${id}" data-label="Prix unitaire">${item.price.viewPrice()}</td>
 								<td class="d-block d-md-table-cell table__td text-end" id="tpi_${id}" data-label="Prix total">${totalPriceItem.viewPrice()}</td>
 								<td class="d-block d-md-table-cell table__td text-end" data-label="Supprimer">
+									<a id="del_${id}" aria-label="supprimer l'article">
 									<svg xmlns="http://www.w3.org/2000/svg"
 										class="bi bi-x-circle text-danger"
-										id="del_${id}"
-										aria-label="supprimer l'article"
 										width="24"
 										height="24"
 										fill="currentColor"
@@ -156,6 +155,7 @@ const render = () => {
 										<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
 										<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
 									</svg>
+									</a>
 								</td>
 							</tr>`;
 		itemsCartElmt.insertAdjacentHTML("beforeend", itemCartElmt);
